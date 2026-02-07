dd97a093ca9bdc4ec557fa6f644c6ae29909cac3
wattox00 <korschzozinho@gmail.com>
2026-02-07 08:27:51 +0100

    deploying

--- np.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.skill-header').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const content = btn.nextElementSibling;
-      content.style.display =
-        content.style.display === 'block' ? 'none' : 'block';
-    });
-  });
